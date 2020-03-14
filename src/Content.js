f6d5e8f0cc960125a410b1a037df42a27fb2932b
Jeffery Gontarek <jeffery@jeffsmac.local>
2020-03-14 12:15:44 -0700

    Components structured

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,13 @@
-mport React from 'react'
+import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Songs from './pages/Songs'
-import Song from './pages/Song'
-import SongEdit from './pages/SongEdit'
-import NotFound from './pages/NotFound'
+import TripsAll from './pages/TripsAll'
+import TripEdit from './pages/TripEdit'
+import TripShow from './pages/TripShow'
 const Content = ()=> {
   return (
     <Switch>
       <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/about">
-        <About />
-      </Route>
-      <Route exact path="/songs">
-        <Songs />
-      </Route>
-      <Route exact path="/songs/:songId">
-        <Song />
-      </Route>
-      <Route  path="/songs/:songId/edit">
-        <SongEdit />
-      </Route>
-      <Route  path="*">
-        <NotFound />
+        <TripsAll />
       </Route>
     </Switch>
   )
