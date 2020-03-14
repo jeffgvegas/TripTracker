5fd2dabf4ccf7f53fa552a11d32f14366f9cf625
Ryan Ketchum <ryan@Ryans-MacBook-Pro-2.local>
2020-03-14 15:13:04 -0700

    Something happening

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Card, ListGroupItem, ListGroup } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Assets/Cards.css";
 
 const TripCard = props => {
+  const [trip, setTrip] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:3001`)
+      .then(res => res.json())
+      .then(data => setTrip(data));
+  }, []);
+
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title>{trip.title}</Card.Title>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroupItem>Start: {props.location}</ListGroupItem>
-        <ListGroupItem>End: {props.location}</ListGroupItem>
+        <ListGroupItem>Start: {trip.location}</ListGroupItem>
+        <ListGroupItem>End: {trip.location}</ListGroupItem>
       </ListGroup>
       <Card.Body>
         <Card.Link href="#">Edit</Card.Link>
